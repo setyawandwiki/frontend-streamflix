c814eb474a45b02ee36d2767ebac030b1c4a65c2
setyawandwiki <setyawandwiki1@gmail.com>
2023-02-07 13:52:00 +0700

    add navbar and background landingpage

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navigations from "./components/Navigations";
+
+function App() {
+  return (
+    <>
+      <div className="myBg">
+        <Navigations />
+      </div>
+    </>
+  );
+}
+
+export default App;
