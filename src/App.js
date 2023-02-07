9a079f3a7b7e28bf74ea74573f293b4ef5b868e7
setyawandwiki <setyawandwiki1@gmail.com>
2023-02-07 13:03:26 +0700

    install eslint

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const test = "dwiki";
+  const dwiki = "awa";
   return (
     <div className="App">
       <header className="App-header">
